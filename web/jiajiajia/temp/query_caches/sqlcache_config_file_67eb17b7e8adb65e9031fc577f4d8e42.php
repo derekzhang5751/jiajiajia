fd20680f2a028853b556fbc4ffde65c34a85ea5a
Derek Zhang <derekzhang5751@gmail.com>
2017-11-07 23:37:38 -0500

    add mobile client

--- web/jiajiajia/temp/query_caches/sqlcache_config_file_67eb17b7e8adb65e9031fc577f4d8e42.php
+++ web/jiajiajia/temp/query_caches/sqlcache_config_file_67eb17b7e8adb65e9031fc577f4d8e42.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1510115349;
+$this->mysql_config_cache_file_time = 1510115709;
 $this->timeline = 0;
 $this->timezone = -18000;
 $this->platform = 'OTHER';
